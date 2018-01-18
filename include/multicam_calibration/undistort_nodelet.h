45e732c5deb571256410dfb52e0604cd44c8f792
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2018-01-18 16:17:06 -0500

    added undistort nodelet, not yet tested

---
+++ include/multicam_calibration/undistort_nodelet.h
@@ -0,0 +1,39 @@
+/* -*-c++-*--------------------------------------------------------------------
+ * 2017 Bernd Pfrommer bernd.pfrommer@gmail.com
+ */
+
+#ifndef MULTICAM_CALIBRATION_UNDISTORT_NODELET_H
+#define MULTICAM_CALIBRATION_UNDISTORT_NODELET_H
+
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <nodelet/nodelet.h>
+#include <ros/ros.h>
+#include <opencv2/core/core.hpp>
+
+
+namespace multicam_calibration {
+  using sensor_msgs::ImageConstPtr;
+  using sensor_msgs::CameraInfoConstPtr;
+  class UndistortNodelet : public nodelet::Nodelet {
+  public:
+    void onInit() override;
+  private:
+    void imageCallback(const ImageConstPtr &img);
+    void cameraInfoCallback(const CameraInfoConstPtr &camInfo);
+    // ---------- variables
+
+    ros::Subscriber             cameraInfoSub_;
+    image_transport::Subscriber imageSub_;
+    image_transport::Publisher  imagePub_;
+
+    cv::Mat mapx_;
+    cv::Mat mapy_;
+  };
+}
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(multicam_calibration::UndistortNodelet, nodelet::Nodelet)
+
+#endif
---
+++ src/undistort_nodelet.cpp
@@ -0,0 +1,42 @@
+/* -*-c++-*--------------------------------------------------------------------
+ * 2017 Bernd Pfrommer bernd.pfrommer@gmail.com
+ */
+#include "multicam_calibration/undistort_nodelet.h"
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/calib3d/calib3d.hpp>
+
+namespace multicam_calibration {
+  void UndistortNodelet::onInit() {
+    ros::NodeHandle nh = getPrivateNodeHandle();
+    cameraInfoSub_ = nh.subscribe("camerainfo", 1,
+                                  &UndistortNodelet::cameraInfoCallback, this);
+    image_transport::ImageTransport it(nh);
+    imageSub_ = it.subscribe("image", 1, &UndistortNodelet::imageCallback, this);
+    imagePub_ = it.advertise("undist_image", 1);
+  }
+
+  void UndistortNodelet::imageCallback(const ImageConstPtr &img) {
+    if (mapx_.rows == 0) {
+      ROS_WARN("no camera info received yet!");
+      return;
+    }
+    cv::Mat im =
+      cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8)->image;
+    cv::Mat rectim;
+    cv::remap(im, rectim, mapx_, mapy_, cv::INTER_LINEAR);
+    imagePub_.publish(cv_bridge::CvImage(img->header, "mono8",
+                                         im).toImageMsg());
+  }
+
+  void UndistortNodelet::cameraInfoCallback(const CameraInfoConstPtr &camInfo) {
+    cv::Mat mapx, mapy;
+    cv::Size new_sz(camInfo->width, camInfo->height);
+    cv::Mat K(3, 3, CV_64F, (void *)&camInfo->K[0]);
+    cv::Mat D(4, 1, CV_64F, (void *)&camInfo->D[0]);
+    cv::Mat R = cv::Mat::eye(3,3, CV_64F);
+    std::cout << K << std::endl;
+    cv::fisheye::initUndistortRectifyMap(K, D, R, K, new_sz,
+                                         CV_32FC1, mapx_, mapy_);
+    cameraInfoSub_.shutdown();
+  }
+}
